212410ce196e81838fec40e729f2c26c91de1b74
andybui2014 <andybui@vsnr.com>
2014-09-19 16:01:07 +0700

    Notifications

--- application/controllers/NotificationsController.php
+++ application/controllers/NotificationsController.php
@@ -14,21 +14,11 @@ class NotificationsController extends Application_Controller_Action
     
     public function listAction()
     {
-        //$this->_helper->layout->disableLayout();
-        //$this->_helper->viewRenderer->setNoRender();
         $request = $this->getRequest();
         $sestionClient = PR_Session::getSession(PR_Session::SESSION_CLIENT);
         $clientID =  $sestionClient['ClientID'];
         $notiClass = new PR_Api_Core_NotiClass();
         $result = $notiClass->getList($clientID);
-       // $result = $result1[0];
-        //$notiID = $result['NotiID'];
-       // $noti = new PR_Api_Noti($notiID);
-       // $result = $noti->getInfo();
-
-           //     echo "<pre>";
-         //print_r($result1);
-        // echo "</pre>";die();
         $this->view->result = $result; //UserName
         $this->view->clientIDLogin = $clientID;
         $this->view->userName = $sestionClient['UserName'];
@@ -50,12 +40,7 @@ class NotificationsController extends Application_Controller_Action
         } else{
             $return = array("success" => 0, "error" => "");
         }
-        //echo "<pre>";
-        // print_r($content);
-        // echo "</pre>";die();
 
-
-        //return
         $response = $this->getResponse();
         $response->clearAllHeaders()->clearBody();
         $return = json_encode($return);
@@ -71,10 +56,7 @@ class NotificationsController extends Application_Controller_Action
         $request = $this->getRequest();
         $content = $request->getParam("editNotification", "");
         $ModalEditNotiID = $request->getParam("ModalEditNotiID", "");
-        //echo "<pre>";
-       // print_r($content); echo "<br>"; echo $ModalEditNotiID;
-       //echo "</pre>";die();
-        //$notiID = 1;
+
         $sestionClient = PR_Session::getSession(PR_Session::SESSION_CLIENT);
         $clientID =  $sestionClient['ClientID'];
         $noti = new PR_Api_Noti($ModalEditNotiID);
