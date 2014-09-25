eed3e6cdd65593685d8525d8c1580bcdb5010719
Hoang <hoangpham@galaxyitsolutions.com>
2014-09-25 16:01:52 +0700

    PR

--- library/PR/Api/Core/NotiClass.php
+++ library/PR/Api/Core/NotiClass.php
@@ -6,15 +6,20 @@ class PR_Api_Core_NotiClass
     {             
         $db = PR_Database::getInstance();
         $select = $db->select();
-        $select->from(array('n'=>'notis'),array('*'));
-        $select->joinLeft(array('c'=>'clients'),
-            'c.ClientID = n.cbClientID',
-            array('cbContactName'=>'ContactName')
+        $select->from(array('n'=>'notification'),array('*'));
+        $select->joinLeft(array('senderuser'=>'user'),
+            'senderuser.UserID = n.sender_iduser',
+            array('cbContactNameT'=>'firstname', 'cbContactLNameT'=>'lastname' )
         );
+        $select->joinLeft(array('receiveduser'=>'user'),
+            'receiveduser.UserID = n.receiver_iduser',
+            array('cbContactNameR'=>'firstname' , 'cbContactLNameR'=>'lastname')
+        );
+
         if(count($filters)>0)
         {
-            if(isset($filters['ClientID'])){
-                $select->where("cbClientID = '".$filters['ClientID']."'");                
+            if(isset($filters)){
+                $select->where("sender_iduser = '".$filters."' || receiver_iduser = '".$filters."'");
             }
         }
         if ( $limit != 0 || $offset != 0)
@@ -30,8 +35,16 @@ class PR_Api_Core_NotiClass
         {
             return;        
         }
-        $criteria = "NotiID IN (".implode(",",$notiIDArray).")";
+        $criteria = "NotificationID IN (".implode(",",$notiIDArray).")";
         $db = PR_Database::getInstance();
-        $result = $db->delete('notis', $criteria);
+        $result = $db->delete('notification', $criteria);
+    }
+    public function geAllUser()
+    {
+        $db = PR_Database::getInstance();
+        $select = $db->select();
+        $select->from(array('u'=>'user'),array('*'));
+        $records = PR_Database::fetchAll($select);
+        return $records;
     }
 }
