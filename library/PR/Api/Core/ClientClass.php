ab1df8b3f6f9a68e59b9e5543b7bd1ac6786b635
Nguyet <nhunguyet_198781@yahoo.com>
2014-10-07 01:19:21 -0800

    pr

--- library/PR/Api/Core/ClientClass.php
+++ library/PR/Api/Core/ClientClass.php
@@ -1,7 +1,7 @@
 <?php
 class PR_Api_Core_ClientClass
 {
-    private $avaiUpdateFields = array('usertype','firstname','middlename','lastname','dob','CompanyID','CandidateProfileID','loginname','password','emailaddress','URL','PhoneNumber','Address1','Address2','City','State','PostalCode','Country','HeardFrom');
+    private $avaiUpdateFields = array('usertype','firstname','middlename','lastname','dob','CompanyID','CandidateProfileID','loginname','password','emailaddress','URL','PhoneNumber','Address1','Address2','City','State','PostalCode','Country','HeardFrom','Role','activate');
     
     public function  __construct() {
         $errMsg="";
@@ -24,6 +24,28 @@ class PR_Api_Core_ClientClass
        //echo ("user:".$user);
                
     }
+    
+     public function getListUsersCompany($companyid)
+    {             
+        $db = PR_Database::getInstance();
+        $select = $db->select();
+         
+        $select = $db->select();
+        $select->from(array('cm'=>'company'),array('CompanyID','Companyname'));
+        $select->join(array('us'=>'user'),
+                    'us.CompanyID = cm.CompanyID',
+                    array("*")
+        );
+        $select->where("us.CompanyID = '".$companyid."' ");
+       
+       // $select->from("user", array("*"));  
+      //  $select->where("usertype = 1");  
+         $record = PR_Database::fetchAll($select);
+        if(empty($record)) return NULL;
+        else return $record;                 
+         
+    }
+    
      public function getListCompany()
     {             
         $db = PR_Database::getInstance();
@@ -37,18 +59,7 @@ class PR_Api_Core_ClientClass
         
         return $companys;
     }
-     public function getCompany($companyid)
-    {             
-       $db = PR_Database::getInstance();
-        $select = $db->select();
-        $select->from("company", array("*"));
-        $select->where("CompanyID = '$companyid'");
-        $record = PR_Database::fetchAll($select);
-        if(empty($record)) return NULL;
-        else return $record[0];        
-    }
-
-    public function getDefaultCompany()
+     public function getDefaultCompany()
     {             
        $db = PR_Database::getInstance();
         $select = $db->select();
@@ -59,6 +70,26 @@ class PR_Api_Core_ClientClass
         else return $record[0];        
     }
     
+     public function getCompany($companyid)
+    {             
+       $db = PR_Database::getInstance();
+        $select = $db->select();
+        $select->from("company", array("*"));
+        $select->where("CompanyID = '$companyid'");
+        $record = PR_Database::fetchAll($select);
+        if(empty($record)) return NULL;
+        else return $record[0];        
+    }
+      public function getCompanyid($userid)
+    {             
+       $db = PR_Database::getInstance();
+        $select = $db->select();
+        $select->from("user", array("*"));
+        $select->where("UserID = '$userid'");
+        $record = PR_Database::fetchAll($select);
+        if(empty($record)) return NULL;
+        else return $record[0];        
+    }
     public function updatecompanyProfile($companyid,$Data)
     {
        $avaiUpdateFields1 = array('Companyname','Industry','Address',
@@ -79,7 +110,76 @@ class PR_Api_Core_ClientClass
         //echo ("criteria:".$criteria);print_r($updateFields);die();
         $result= PR_Database::update('company',$updateFields,$criteria);
         return $result;
+       //echo ("user:".$user);
+
     }
+     public function updateUserProfile($userid,$Data)
+        {
+            if(count($userid)==0){
+                return;
+            }
+            //echo ("Data:");print_r($Data);die();
+            $updateFields = array();
+            foreach($Data as $key=>$value){
+                if (in_array($key,$this->avaiUpdateFields)) {
+                    $updateFields[$key] = $value;
+                }            
+            }
+
+            $criteria = "UserID = '$userid'";
+           // echo ("criteria:".$criteria);print_r($updateFields);die();
+           $result= PR_Database::update('user',$updateFields,$criteria);
+           return $result;
+           //echo ("user:".$user);
+                   
+        }
+        public function deleteUser($userid)
+        {
+             $db = PR_Database::getInstance();
+             $criteria = "UserID = '$userid'";
+             $result = $db->delete('user', $criteria);
+                    
+        }
+         public function AddUser($data)
+            {
+                    
+                    if(empty($data) || !is_array($data)) return NULL;
+
+                    $db = PR_Database::getInstance();
+                    $Fields = array();   
+                   
+                   foreach($data as $key=>$value){
+                    if (in_array($key,$this->avaiUpdateFields)) {
+                        $Fields[$key] = $value;
+                    }            
+                   }              
+                  
+                    //$api=new PR_Api_Core_ClientClass();
+                    $defaultCompany=$this->getDefaultCompany();
+                    $defaultCompanyID = $defaultCompany['CompanyID'];
+                    $primaryEmail=$data["emailaddress"];    
+                    $maxIdSql = "SELECT MAX(UserID) AS UserID  FROM user";
+                    $result = $db->fetchAll($maxIdSql);
+                    $User_ID=$result[0]['UserID']+1;
+                    $select = $db->select()->from(PR_Database::TABLE_USER, 
+                                        array('emailaddress'));
+                        $select->where("emailaddress = '$primaryEmail'");
+                        $res = $db->fetchAll($select);
+                        
+                        if(!empty($res) && count($res) > 0 ) {
+                            
+                            return array("error" => "email exists");
+                            echo("email exists");
+                        } 
+                       
+                        else
+                        {
+                                $updateFields=array('UserID'=>$User_ID,'usertype'=>'1','firstname'=>$Fields['firstname'],'lastname'=>$Fields['lastname'],"emailaddress" => $Fields['emailaddress'],"Address1" => $Fields['Address1'],"password" => $Fields['password'],"CompanyID"=>$defaultCompanyID,"PhoneNumber"=>$Fields['PhoneNumber'],"URL"=>$Fields['URL'],"City"=>$Fields['City'],"Country"=>$Fields['Country'],"PostalCode"=>$Fields['PostalCode']);
+                                 $result = PR_Database::insert("user", $updateFields);
+                        
+                }
+            }
+             
 
         
 }  
