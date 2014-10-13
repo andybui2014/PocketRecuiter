e0672013b8c84ca355c52430f07843e6df45015e
VietNguyen <mr.huyviet@gmail.com>
2014-10-13 16:38:05 +0700

    pr

--- application/controllers/CandidateController.php
+++ application/controllers/CandidateController.php
@@ -27,6 +27,43 @@ class CandidateController extends Application_Controller_Action
         $this->_helper->redirector("index","login");
     }
 
+    public function detailEmploymentAction(){
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        $ajaxRes = array('success'=>0,'info'=>null);
+        if($this->getRequest()->isXmlHttpRequest()){
+            $params = $this->getRequest()->getParams();
+            $id = isset($params['id']) ? $params['id'] : null;
+            $core = new PR_Api_Core_CandidateClass();
+            $ajaxRes['info'] = $core->getCandidateEmployment($id);
+            //echo '<pre>';
+            //print_r($ajaxRes['info']);
+            //echo '</pre>';
+            //die();
+
+            //Convert m/d/Y StartDate
+            $startDate = $ajaxRes['info']['StartDate'];
+            if (($timestamp = strtotime($startDate)) !== false)
+                $startDate = date("m/d/Y", $timestamp);
+            $ajaxRes['info']['StartDate'] = $startDate;
+
+            //Convert m/d/Y EndDate
+            $endDate = $ajaxRes['info']['EndDate'];
+            if (($timestamp = strtotime($endDate)) !== false)
+                $endDate = date("m/d/Y", $timestamp);
+            $ajaxRes['info']['EndDate'] = $endDate;
+
+            if(sizeof($ajaxRes['info']) > 0) $ajaxRes['success'] = 1;
+
+        }
+        $response = $this->getResponse();
+        $response->clearAllHeaders()->clearBody();
+        $ajaxRes = json_encode($ajaxRes);
+        $response->setHeader('Content-type', 'application/json');
+        $response->setHeader('Content-Length', strlen($ajaxRes), true)
+            ->setBody($ajaxRes);
+    }
+
     public function profileBuilderAction(){
         $params = $this->getRequest()->getParams();
         if(!empty($params) && isset($params['utm_source'])){
@@ -48,6 +85,9 @@ class CandidateController extends Application_Controller_Action
                     $this->render('profile-builder/education');
                     break;
                 case 'employment':
+                    $core = new PR_Api_Core_CandidateClass();
+                    $list = $core->getCandidateEmployments($client['UserID']);
+                    $this->view->list = $list;
                     $this->view->stepCount = '3/5 Steps';
                     $this->render('profile-builder/employment');
                     break;
@@ -56,6 +96,9 @@ class CandidateController extends Application_Controller_Action
                     $this->render('profile-builder/skills');
                     break;
                 case 'portfolio':
+                    $core = new PR_Api_Core_CandidateClass();
+                    $list = $core->getListCandidatePortfolio($client['UserID']);
+                    $this->view->list = $list;
                     $this->view->stepCount = '5/5 Steps';
                     $this->render('profile-builder/portfolio');
                     break;
@@ -73,7 +116,6 @@ class CandidateController extends Application_Controller_Action
     }
 
     public function startProfileAction(){
-
 
     }
     public function stepNextContactAction(){
@@ -110,10 +152,185 @@ class CandidateController extends Application_Controller_Action
             ->setBody($ajaxRes);
     }
 
-   
-    
-    public function profileAction()
-	{
+    public function doDeletePortfolioAction(){
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        $ajaxRes = array('success'=>0,'info'=>null);
+        if($this->getRequest()->isXmlHttpRequest()){
+            $params = $this->getRequest()->getParams();
+            $core = new PR_Api_Core_CandidateClass();
+            if(!empty($params['data']) && sizeof($params['data']) > 0){
+                foreach($params['data'] as $key=>$value){
+                    $core->deleteCandidatePortfolio($value);
+                }
+            }
+        }
+        $ajaxRes['success'] = 1;
+        $response = $this->getResponse();
+        $response->clearAllHeaders()->clearBody();
+        $ajaxRes = json_encode($ajaxRes);
+        $response->setHeader('Content-type', 'application/json');
+        $response->setHeader('Content-Length', strlen($ajaxRes), true)
+            ->setBody($ajaxRes);
+    }
+
+    public function doAddPortfolioAction(){
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        $ajaxRes = array('success'=>0,'info'=>null);
+        if($this->getRequest()->isXmlHttpRequest()){
+            $params = $this->getRequest()->getParams();
+
+            $title = null;
+            $url = null;
+            $description = null;
+
+            $client = PR_Session::getSession(PR_Session::SESSION_USER);
+
+            foreach($params['data'] as $item){
+                if($item['name']=='title')         $title = $item['value'];
+                if($item['name']=='url')           $url = trim($item['value']);
+                if($item['name']=='description')   $description = $item['value'];
+            }
+
+            $core = new PR_Api_Core_CandidateClass();
+            if(!empty($title)){
+                $isSuccess = $core->addCandidatePortfolio($client['UserID'],$title,$url,$description,null);
+                if($isSuccess) $ajaxRes['success'] = 1;
+            }
+
+        }
+        $response = $this->getResponse();
+        $response->clearAllHeaders()->clearBody();
+        $ajaxRes = json_encode($ajaxRes);
+        $response->setHeader('Content-type', 'application/json');
+        $response->setHeader('Content-Length', strlen($ajaxRes), true)
+            ->setBody($ajaxRes);
+    }
+    public function doAddEducationAction(){
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        $ajaxRes = array('success'=>0,'info'=>null);
+        if($this->getRequest()->isXmlHttpRequest()){
+            $params = $this->getRequest()->getParams();
+            if(!empty($params['data']) && sizeof($params['data'])){
+                $client = PR_Session::getSession(PR_Session::SESSION_USER);
+
+                $instName = null;
+                $degreeName = null;
+                $startYear = null;
+                $endYear = null;
+
+                foreach($params['data'] as $item){
+                    if($item['name']=='inst-name')      $instName = $item['value'];
+                    if($item['name']=='degree-name')    $degreeName = $item['value'];
+                    if($item['name']=='start-year')     $startYear = $item['value'];
+                    if($item['name']=='end-year')       $endYear = $item['value'];
+                }
+                $core = new PR_Api_Core_CandidateClass();
+                $isSuccess = $core->addCandidateEducation($client['UserID'],$instName,$degreeName,$startYear,$endYear);
+
+                if($isSuccess) $ajaxRes['success'] = 1;
+            }
+        }
+        $response = $this->getResponse();
+        $response->clearAllHeaders()->clearBody();
+        $ajaxRes = json_encode($ajaxRes);
+        $response->setHeader('Content-type', 'application/json');
+        $response->setHeader('Content-Length', strlen($ajaxRes), true)
+            ->setBody($ajaxRes);
+    }
+    public function doRemoveEmploymentAction(){
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        $ajaxRes = array('success'=>0,'info'=>null);
+        if($this->getRequest()->isXmlHttpRequest()){
+            $params = $this->getRequest()->getParams();
+            $id = $params['id'];
+            $core = new PR_Api_Core_CandidateClass();
+            $core->deleteCandidateEmployment($id);
+            $ajaxRes['success'] = 1;
+        }
+        $response = $this->getResponse();
+        $response->clearAllHeaders()->clearBody();
+        $ajaxRes = json_encode($ajaxRes);
+        $response->setHeader('Content-type', 'application/json');
+        $response->setHeader('Content-Length', strlen($ajaxRes), true)
+            ->setBody($ajaxRes);
+    }
+
+    public function doUpdateEmploymentAction(){
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        $ajaxRes = array('success'=>0,'info'=>null);
+        if($this->getRequest()->isXmlHttpRequest()){
+            $params = $this->getRequest()->getParams();
+            $empId = null;
+            $companyName = null;
+            $positionHeld = null;
+            $startDate = null;
+            $endDate = null;
+            $description = null;
+
+            if(!empty($params['data']) && sizeof($params['data'])){
+
+                foreach($params['data'] as $key=>$item){
+                    if($item['name']=='empId')          $empId = $item['value'];
+                    if($item['name']=='companyName')    $companyName  = $item['value'];
+                    if($item['name']=='posotionHeld')   $positionHeld = $item['value'];
+                    if($item['name']=='startDate')      $startDate = $item['value'];
+                    if($item['name']=='endDate')        $endDate = $item['value'];
+                    if($item['name']=='description')    $description = $item['value'];
+                }
+                $client = PR_Session::getSession(PR_Session::SESSION_USER);
+                $core = new PR_Api_Core_CandidateClass();
+                $isSuccess = $core->updateCandidateEmployment($empId,$companyName,$positionHeld,$startDate,$endDate,$description);
+                if($isSuccess) $ajaxRes['success'] = 1;
+            }
+        }
+        $response = $this->getResponse();
+        $response->clearAllHeaders()->clearBody();
+        $ajaxRes = json_encode($ajaxRes);
+        $response->setHeader('Content-type', 'application/json');
+        $response->setHeader('Content-Length', strlen($ajaxRes), true)
+            ->setBody($ajaxRes);
+    }
+    public function doAddEmploymentAction(){
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        $ajaxRes = array('success'=>0,'info'=>null);
+        if($this->getRequest()->isXmlHttpRequest()){
+            $params = $this->getRequest()->getParams();
+
+            $companyName = null;
+            $posotionHeld = null;
+            $startDate = null;
+            $endDate = null;
+            $description = null;
+
+            if(!empty($params['data']) && sizeof($params['data'])){
+
+                foreach($params['data'] as $key=>$item){
+                    if($item['name']=='companyName')    $companyName  = $item['value'];
+                    if($item['name']=='posotionHeld')   $posotionHeld = $item['value'];
+                    if($item['name']=='startDate')      $startDate = $item['value'];
+                    if($item['name']=='endDate')        $endDate = $item['value'];
+                    if($item['name']=='description')    $description = $item['value'];
+                }
+                $client = PR_Session::getSession(PR_Session::SESSION_USER);
+                $core = new PR_Api_Core_CandidateClass();
+                $isSuccess = $core->addCandidateEmployment($client['UserID'],$companyName,$posotionHeld,$startDate,$endDate,$description);
+                if($isSuccess) $ajaxRes['success'] = 1;
+            }
+        }
+        $response = $this->getResponse();
+        $response->clearAllHeaders()->clearBody();
+        $ajaxRes = json_encode($ajaxRes);
+        $response->setHeader('Content-type', 'application/json');
+        $response->setHeader('Content-Length', strlen($ajaxRes), true)
+            ->setBody($ajaxRes);
+    }
+    public function profileAction(){
         $client = PR_Session::getSession(PR_Session::SESSION_USER);
         $UserID=$client["UserID"];
         $emailaddress = $client["emailaddress"];
@@ -126,13 +343,12 @@ class CandidateController extends Application_Controller_Action
         $Candidateprofile_ID=$getUserArray["CandidateProfileID"];
         $getCandidates=$api_candidate->getCandidateProfile($Candidateprofile_ID);
         $this->view->getCandidates=$getCandidates;
-        $SkillName=$api_candidate->getList_CandidateSkills($UserID);
-        $Skills=array();
-        foreach($SkillName as $key=>$values )
+        $SkillName=array();
+        foreach($getCandidates["SkillName"] as $key=>$values )
         {
-           $Skills[$key]=$values;   
+            $SkillName[$key]=$values;
         }
-        $this->view->SkillName=$Skills;
+        $this->view->SkillName=$SkillName;
         $CandidateEmployment=array();
         foreach($getCandidates["CandidateEmploymentID"] as $key=>$values )
         {
@@ -141,15 +357,13 @@ class CandidateController extends Application_Controller_Action
         $this->view->CandidateEmployment=$CandidateEmployment;
         $Education = $api_candidate->getCandidateEducationList(2); 
         $this->view->Education=$Education;
-       // $a=$api_candidate->getList_CandidateSkills($UserID) ; 
-       //  echo("test:<pre>");print_r($a);echo("</pre>"); 
-      //  echo ("getUserArray:<pre>");print_r($getCandidates);echo("</pre>");
+
+        // echo ("getUserArray:<pre>");print_r($Education);echo("</pre>");
         $this->render('profile');
 		 
         
         
         
-     
-}
+    }
 
 }
