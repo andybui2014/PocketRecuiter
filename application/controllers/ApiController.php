0ba9d222bc1a319ff5d898ffc174f379410309c3
Hoang <hoangpham@galaxyitsolutions.com>
2014-10-17 17:34:04 +0700

    pr

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -110,22 +110,16 @@ class ApiController extends Application_Controller_Action
                 $edit_PR_Api = new PR_Api_Core_CareerClass();
                 $edit_PR_Api->saveCareerSkills($OpportunityID, $SkillID);
                 $edit_PR_Api->saveCareerTests($OpportunityID, $testid) ;
-                $return.="<opp>
-                            <oppotunityID>" . ($result->OpportunityID !=0 ? "Success" : "No") . "</oppotunityID>
-                       </opp>";
-
-
-
-                $return = "
-                <result>
-                    <opportunity>" . $return . "</opportunity>
-                </result>";
+                $return = "<result>
+	                <success>true</success>
+                    <errors></errors>
+                    </result>";
             } else{
-                $return = "<result>no result</result>";
+                $return = "<result><success>false</success> <errors><error>Can not create the opportunity.</error></errors></result>";
             }
         } else {
             $return = "
-                    <result>
+                    <result><success>false</success>
             <errors>";
             $i = 0;
             foreach ($errs as $err)
@@ -142,135 +136,6 @@ class ApiController extends Application_Controller_Action
 
     }
 
-    public function addOpportunityByGuiAction()
-    {
-        $this->_helper->layout->disableLayout();
-        $BYTES =8;
-        $token  = openssl_random_pseudo_bytes($BYTES);
-
-        $username = $_POST['Email'];
-        $password = $_POST['Password'];
-        $errs = array();
-        $userApi = new PR_Api_User();
-        $authData = array('emailaddress' => $username, 'password' =>$password);
-        if ($userApi->loadAndCheckAuthentication($authData))
-        {
-        } else{
-            $errs[] =  array("err" => "Username and password must be required.");
-        }
-
-        $postedby = $_POST['UserID'];
-
-        $posteddate = "";
-        $title = $_POST['CareerName'];
-
-        //$companyname = $_REQUEST['companyname'];
-
-        $OppCompanyID = $_POST['CompanyID'];
-        $careerdescription = $_POST['CareerDescription'];
-        $status = $_POST['Status'];
-        $industry = $_POST['Industry'];
-        $jobtype =$_POST['JobType'];
-        $duration =$_POST['Duration'];
-        $location =$_POST['Location'];
-        $zipcode =$_POST['ZipCode'];
-        $minimuneducation =$_POST['MinimunEducation'];
-        $degreetitle =$_POST['DegreeTitle'];
-        $StaffFavorite ="";
-        $SkillID = array();
-        $SkillID[] =$_POST['SkillID'];  //array
-
-        $requiredExperience =$_POST['RequiredExperience'];
-        $salaryRangeF =$_POST['SalaryRangeF'];
-        $salaryRangeT =$_POST['SalaryRangeT'];
-        $testid = array();
-        $testid[] =$_POST['TestID'];  //array
-
-        $return = "";
-        if($title==""){
-            $errs[] = array("err" => "Career Name cannot be empty.");
-        }
-
-        if($postedby==""){
-            $errs[] = array("err" => "UserID cannot be empty.");
-        }
-
-        if($careerdescription==""){
-            $errs[] = array("err" => "Career description cannot be empty.");
-        }
-
-        if($industry==""){
-            $errs[] = array("err" => "Career Industry cannot be empty.");
-        }
-
-        if($minimuneducation==""){
-            $errs[] = array("err" => "Minimun Education cannot be empty.");
-        }
-
-        if($degreetitle==""){
-            $errs[] = array("err" => "Degree Title cannot be empty.");
-        }
-
-        if($SkillID==""){
-            $errs[] = array("err" => "Required Skills cannot be empty.");
-        }
-
-        if($requiredExperience==""){
-            $errs[] = array("err" => "Required Experience cannot be empty.");
-        }
-
-        if($salaryRangeF=="" || $salaryRangeT ==""){
-            $errs[] = array("err" => "Salary Range cannot be empty.");
-        }
-
-        if (empty($errs))
-        {
-            $career_PR_Api = new PR_Api_Career(null);
-
-            $updateFields = array('CompanyID'=>$OppCompanyID,'postedby'=>$postedby, 'posteddate'=>$posteddate,
-                'title'=>$title,'careerdescription'=>$careerdescription,'status'=>$status,'industry'=>$industry,'industry'=>$industry,'jobtype'=>$jobtype,
-                'duration'=>$duration,'location'=>$location,'zipcode'=>$zipcode,'minimuneducation'=>$minimuneducation,'degreetitle'=>$degreetitle,'StaffFavorite'=>$StaffFavorite,
-                'salaryrangefrom'=>$salaryRangeF, 'salaryrangeto'=>$salaryRangeT,'experienced'=>$requiredExperience
-            );
-
-            $result = $career_PR_Api->saveCareer($updateFields);
-
-            if($result){
-                $OpportunityID = $result->OpportunityID;
-                $edit_PR_Api = new PR_Api_Core_CareerClass();
-                $edit_PR_Api->saveCareerSkills($OpportunityID, $SkillID);
-                $edit_PR_Api->saveCareerTests($OpportunityID, $testid) ;
-                $return.="<opp>
-                            <oppotunityID>" . ($result->OpportunityID !=0 ? "Success" : "No") . "</oppotunityID>
-                       </opp>";
-
-
-
-                $return = "
-                <result>
-                    <opportunity>" . $return . "</opportunity>
-                </result>";
-            } else{
-                $return = "<result>no result</result>";
-            }
-        } else {
-            $return = "
-                    <result>
-            <errors>";
-            $i = 0;
-            foreach ($errs as $err)
-            {
-                $return .="<error>" . $err['err'] . "</error>";
-            }
-
-            $return .="</errors></result>";
-        }
-        header('Content-type: application/xml');
-        header("Content-Length: " . strlen($return));
-        print_r($return);
-
-
-    }
 
     public function testoppAction()
     {
